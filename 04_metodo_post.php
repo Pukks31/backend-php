5a785be7563c7a2d90576b9fb27982db823e3ede
Carlos Agudelo <charlidagudelo@gmail.com>
2023-08-15 19:02:57 -0500

    variables y metodos

---
+++ 04_metodo_post.php
@@ -0,0 +1,30 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Digite la siguiente info: </h1>
+    <form action="03_metodo_get.php" method="post"></form>
+    Nombre:
+    <input type="text" name="Nombre"/>
+    <br>
+    Fecha:
+    <input type="date" name="Fecha" id=""/>
+    <br>
+    Observaciones:
+    <input type="text" name="obs" id=""/>
+    <br>
+    <input type="submit" value="enviar"/>
+</form>
+<h2>Informacion enviada es: </h2>
+<?php
+print_r($_GET);
+?>
+</body>
+</html>
---
+++ Variables.php
@@ -0,0 +1,15 @@
+<?php
+//tipos de variables//
+    $nombre = "Mateo";
+    $edad = 23;
+    $double = 25.6;
+    $boolean = false;
+    $nombrecurso = "Backend";
+
+    //concatenaciones
+    $concatetaciones = $nombre . '<br>' . $edad . '<br>' . '<br>'. $double;
+
+
+    echo $concatetaciones;
+    ?>
+    
